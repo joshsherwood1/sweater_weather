fe388ca16396f7e15fa5aa50f65903210e5a4b10
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 15:21:42 -0700

    Update request spec for getting background image with appropriate tests to test json

--- spec/requests/api/v1/background_spec.rb
+++ spec/requests/api/v1/background_spec.rb
@@ -1,18 +1,15 @@
 require 'rails_helper'
 
-describe "Weather API" do
-  it "sends a list of customers" do
+describe "Background API" do
+  it "sends url for a picture of specified city", :vcr do
 
     get '/api/v1/backgrounds?location=denver,co'
 
     expect(response).to be_successful
 
     data = JSON.parse(response.body)
-    expect(data.count).to eq(3)
-    expect(data["current_weather"].count).to eq(8)
-    expect(data["details"].count).to eq(8)
-    expect(data["forecast"].count).to eq(2)
-    expect(data["forecast"]["hourly"].count).to eq(8)
-    expect(data["forecast"]["daily"].count).to eq(5)
+    expect(data.count).to eq(1)
+    expect(data["data"]["attributes"].count).to eq(1)
+    expect(data["data"].count).to eq(2)
   end
 end
