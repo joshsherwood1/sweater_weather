7f07970dd0ddc990216e3df47a494599c39ee8ee
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 15:25:05 -0700

    Update show action in picture facade to instantiate new picturefacade object and render that information with picture serializer

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
-    @picture = PictureFacade.new(params[:location]).get_picture
-  #   @coordinates = CoordinateFacade.new(params[:location]).get_coordinates
-  #   @forecast_data = ForecastFacade.new(@coordinates).get_forecast
-  #   render json: ForecastSerializer.new(params[:location], @forecast_data).json
+    @picture_url = PictureFacade.new(params[:location]).get_picture
+    render json: PictureSerializer.new(@picture_url).json
   end
 end
