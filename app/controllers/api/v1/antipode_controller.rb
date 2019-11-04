7bd35c0138a7716f84bb1b33a7a5cdd21ba25510
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 10:09:24 -0700

    Add line in show action in antipode controller that create location facade with antipode coordinates

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::AntipodeController < ApplicationController
   def show
     @coordinates = CoordinateFacade.new(params[:location]).get_coordinates
     @antipode_coordinates = AntipodeFacade.new(@coordinates).get_coordinates
-    @forecast_data = ForecastFacade.new(@antipode_coordinates).get_forecast
+    @forecast_data = ForecastFacade.new(@antipode_coordinates).get_antipode_forecast
+    @antipode_coordinates_location = LocationFacade.new(@antipode_coordinates).get_location
     # render json: ForecastSerializer.new(params[:location], @forecast_data).json
   end
 end
