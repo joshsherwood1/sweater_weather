ab4562293b5c10aa5511c6da37429681099bbc47
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-03 21:39:45 -0700

    Create request spec for getting forecast for denver, co

---
+++ spec/requests/api/v1/weather_data_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Weather API" do
+  it "sends a list of customers" do
+
+    get '/api/v1/forecast?location=denver,co'
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body)
+    expect(data.count).to eq(3)
+    expect(data["current_weather"].count).to eq(8)
+    expect(data["details"].count).to eq(8)
+    expect(data["forecast"].count).to eq(2)
+    expect(data["forecast"]["hourly"].count).to eq(8)
+    expect(data["forecast"]["daily"].count).to eq(5)
+  end
+end
