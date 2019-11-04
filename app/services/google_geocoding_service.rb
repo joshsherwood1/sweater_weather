223d213a7bd78cd31a72cf5fe60492db95b5cc7f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 10:10:48 -0700

    Update google geocoding service so that it can get location with coordinates

--- app/services/google_geocoding_service.rb
+++ app/services/google_geocoding_service.rb
@@ -1,12 +1,21 @@
 class GoogleGeocodingService
-  def initialize(location)
-    @location = location
+  def initialize(data)
+    @data = data
   end
 
   def coordinates
     response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json") do |req|
       req.params["key"] = ENV['GOOGLE_API_KEY']
-      req.params["address"] = @location
+      req.params["address"] = @data
+    end
+    parse_json(response)
+  end
+
+  def location
+    response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json") do |req|
+      req.params["key"] = ENV['GOOGLE_API_KEY']
+      req.params["latlng"] = "#{@data[:lat]},#{@data[:long]}"
+      req.params["result_type"] = "country"
     end
     parse_json(response)
   end
