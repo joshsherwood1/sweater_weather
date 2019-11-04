ba89559fbf027c627f5a532fedabacfa6c6bda8b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 11:43:48 -0700

    Refactor information facade to apply encapsulation by creating private methods to create new facade objects

--- app/facades/information_facade.rb
+++ app/facades/information_facade.rb
@@ -4,15 +4,29 @@ class InformationFacade
   end
 
   def get_information
-    @coordinates = CoordinateFacade.new(@location).get_coordinates
-    @forecast_data =  get_forecast_data_for_antipode_of_coordiantes(@coordinates)
-    @antipode_location = LocationFacade.new(@antipode_coordinates).get_location
-    [@forecast_data, @antipode_location, @location]
+    coordinates = obtain_coordinates(@location)
+    antipode_coordinates = obtain_antipode_coordinates(coordinates)
+    forecast_data =  get_forecast_data_for_antipode_of_coordiantes(antipode_coordinates)
+    antipode_location = obtain_location(antipode_coordinates)
+    [forecast_data, antipode_location, @location]
   end
 
-  def get_forecast_data_for_antipode_of_coordiantes(coordinates)
-    @antipode_coordinates = AntipodeFacade.new(@coordinates).get_coordinates
-    ForecastFacade.new(@antipode_coordinates).get_antipode_forecast
+  private
+  attr_reader :location
+  
+  def get_forecast_data_for_antipode_of_coordiantes(antipode_coordinates)
+    ForecastFacade.new(antipode_coordinates).get_antipode_forecast
   end
 
+  def obtain_antipode_coordinates(coordinates)
+    AntipodeFacade.new(coordinates).get_coordinates
+  end
+
+  def obtain_coordinates(location)
+    CoordinateFacade.new(@location).get_coordinates
+  end
+
+  def obtain_location(antipode_coordinates)
+    LocationFacade.new(antipode_coordinates).get_location
+  end
 end
