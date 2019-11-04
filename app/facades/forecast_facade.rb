f2e5d1d41f1efa4f081c6216530589c7cc04a4a8
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-03 21:40:38 -0700

    Create forecast and coordinate facade

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,14 @@
+class ForecastFacade
+  def initialize(coordinates)
+    @coordinates = coordinates
+  end
+
+  def get_forecast
+    data = get_forecast_for_city(@coordinates[:lat], @coordinates[:lng])
+    [CurrentWeather.new(data), Details.new(data), data]
+  end
+
+  def get_forecast_for_city(latitude, longitude)
+    DarkSkyService.new(latitude, longitude).forecast
+  end
+end
