05e8551258d48bb0b4bc360fb4b6a910186c5a5b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 09:40:18 -0700

    Add code in show action in antipode controller that will generate request to create new forecast facade with antipode coordinates

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AntipodeController < ApplicationController
   def show
     @coordinates = CoordinateFacade.new(params[:location]).get_coordinates
     @antipode_coordinates = AntipodeFacade.new(@coordinates).get_coordinates
-    # @forecast_data = ForecastFacade.new(@coordinates).get_forecast
+    @forecast_data = ForecastFacade.new(@antipode_coordinates).get_forecast
     # render json: ForecastSerializer.new(params[:location], @forecast_data).json
   end
 end
