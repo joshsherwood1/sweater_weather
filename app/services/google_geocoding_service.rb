e53778e106f4cddc28e5d84a9ce370871b8d3238
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 11:27:14 -0700

    Refactor google geocoding api to keep in mind encapsulation by creating private method conn and trimming down existing methods coordiantes and location

--- app/services/google_geocoding_service.rb
+++ app/services/google_geocoding_service.rb
@@ -4,23 +4,22 @@ class GoogleGeocodingService
   end
 
   def coordinates
-    response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json") do |req|
-      req.params["key"] = ENV['GOOGLE_API_KEY']
-      req.params["address"] = @data
-    end
-    parse_json(response)
+    parse_json(conn.get("", address: @data, key: ENV['GOOGLE_API_KEY']))
   end
 
   def location
-    response = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json") do |req|
-      req.params["key"] = ENV['GOOGLE_API_KEY']
-      req.params["latlng"] = "#{@data[:lat]},#{@data[:long]}"
-      req.params["result_type"] = "country"
-    end
-    parse_json(response)
+    parse_json(conn.get("", latlng: "#{@data[:lat]},#{@data[:long]}",
+                            key: ENV['GOOGLE_API_KEY'], result_type: "country"))
   end
 
   private
+
+  def conn
+    Faraday.new(url: 'https://maps.googleapis.com/maps/api/geocode/json') do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
   def parse_json(response)
     JSON.parse(response.body, symbolize_names: true)
   end
