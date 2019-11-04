2722d54e4180fd9af4d5c218747eef4625c10ff9
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 11:03:19 -0700

    Refactor show action in antipode controller so that all facade initializations are moved to information facade

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::AntipodeController < ApplicationController
   def show
-    @coordinates = CoordinateFacade.new(params[:location]).get_coordinates
-    @antipode_coordinates = AntipodeFacade.new(@coordinates).get_coordinates
-    @forecast_data = ForecastFacade.new(@antipode_coordinates).get_antipode_forecast
-    @antipode_coordinates_location = LocationFacade.new(@antipode_coordinates).get_location
-    render json: AntipodeSerializer.new(@forecast_data, @antipode_coordinates_location, params[:location]).json
+    @information = InformationFacade.new(params[:location]).get_information
+    render json: AntipodeSerializer.new(@information).json
   end
 end
