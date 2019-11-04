802abb10fcd6d89cc9f45d06fbee3cd140738c0b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 10:30:14 -0700

    Update antipode serializer in show action in antipode controller so that it takes in location as well

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -4,6 +4,6 @@ class Api::V1::AntipodeController < ApplicationController
     @antipode_coordinates = AntipodeFacade.new(@coordinates).get_coordinates
     @forecast_data = ForecastFacade.new(@antipode_coordinates).get_antipode_forecast
     @antipode_coordinates_location = LocationFacade.new(@antipode_coordinates).get_location
-    render json: AntipodeSerializer.new(@forecast_data, @antipode_coordinates_location).json
+    render json: AntipodeSerializer.new(@forecast_data, @antipode_coordinates_location, params[:location]).json
   end
 end
