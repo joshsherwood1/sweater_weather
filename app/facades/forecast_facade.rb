89397ef392628e458b1c6e0a87c8cc838d838518
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 10:10:13 -0700

    Update forecast facade with method that gets forecast when specifically given antipode coordinates

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -8,6 +8,11 @@ class ForecastFacade
     [CurrentWeather.new(data), Details.new(data), data]
   end
 
+  def get_antipode_forecast
+    data = get_forecast_for_city(@coordinates[:lat], @coordinates[:long])
+    CurrentWeather.new(data)
+  end
+
   def get_forecast_for_city(latitude, longitude)
     DarkSkyService.new(latitude, longitude).forecast
   end
