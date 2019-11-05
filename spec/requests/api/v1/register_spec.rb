9b6308c776feb760d072b1ea5153e9c4989b75e9
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-04 22:58:26 -0700

    Create request spec for creating account and account for multiple sad paths

---
+++ spec/requests/api/v1/register_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe "Register endpoint" do
+  it "user can create account and receive access token" do
+
+    post '/api/v1/users', :params => { "email" => "whatever@example.com", "password" => "password", "password_confirmation" => "password" }
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body)
+    expect(data.count).to eq(2)
+    expect(data["status"]).to eq(201)
+    expect(data["body"].keys).to eq(["api_key"])
+    expect(data["body"].count).to eq(1)
+  end
+
+  it "user cannot create account when passwords do not match" do
+
+    post '/api/v1/users', :params => { "email" => "whatever@example.com", "password" => "password", "password_confirmation" => "password123456" }
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body)
+    expect(data.count).to eq(2)
+    expect(data["status"]).to eq(422)
+    expect(data["body"]["msg"]).to eq("Password confirmation doesn't match Password and Password confirmation doesn't match Password")
+  end
+
+  it "user cannot create account when email has already been taken" do
+
+    post '/api/v1/users', :params => { "email" => "whatever@example.com", "password" => "password", "password_confirmation" => "password" }
+    post '/api/v1/users', :params => { "email" => "whatever@example.com", "password" => "password", "password_confirmation" => "password" }
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body)
+    expect(data.count).to eq(2)
+    expect(data["status"]).to eq(422)
+    expect(data["body"]["msg"]).to eq("Email has already been taken")
+  end
+
+  it "user cannot create account when password does not meet requirements" do
+
+    post '/api/v1/users', :params => { "email" => "whatever@example.com", "password" => "pass", "password_confirmation" => "pass" }
+
+    expect(response).to be_successful
+    data = JSON.parse(response.body)
+    expect(data.count).to eq(2)
+    expect(data["status"]).to eq(422)
+    expect(data["body"]["msg"]).to eq("Password is too short (minimum is 8 characters)")
+  end
+end
