4991f699d95feab8052e0452beef8d8c3bcc7ff3
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-05 13:57:52 -0700

    Add user.save to create method in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,13 @@ require 'securerandom'
 
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
-  
+
   def create
     information = JSON.parse(request.body.read, symbolize_names: true)
     user = User.new(information)
     if user.save
       user.api_key = SecureRandom.urlsafe_base64
+      user.save
       render json: UserSerializer.new(user.api_key).json
     else
       render json: ErrorSerializer.new(user.errors.full_messages.to_sentence).json
