dd5c0f82ed215e74d30ef4b7bcb4bebc97c36ea6
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-05 13:54:54 -0700

    Add skip_before_action :verify_authenticity_token to sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
-  # skip_before_action :verify_authenticity_token, only: [:create]
-
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
     information = JSON.parse(request.body.read, symbolize_names: true)
@@ -12,15 +11,4 @@ class Api::V1::SessionsController < ApplicationController
       render json: ErrorSerializer.new("Login information incorrect").json
     end
   end
-
-  # def create
-  #   information = JSON.parse(request.body.read, symbolize_names: true)
-  #   user = User.new(information)
-  #   if user.save
-  #     user.api_key = SecureRandom.urlsafe_base64
-  #     render json: UserSerializer.new(user.api_key).json
-  #   else
-  #     render json: ErrorSerializer.new(user.errors.full_messages.to_sentence).json
-  #   end
-  # end
 end
